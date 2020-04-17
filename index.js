1c0393531d9877b9d4dfc3cc840b088ad6b08e6b
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-17 20:16:58 +0200

    Restructuring the project

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const express = require('express');
 const app = express();
 
 mongoose.connect('mongodb://localhost/vidly')
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...'));
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
 app.use('/api/genres', genres);
