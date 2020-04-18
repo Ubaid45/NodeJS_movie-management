65e3a7b1c708bfe06fc04fc84d6e7fea0ecd05ae
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-18 23:59:11 +0200

    Express error middleware

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const error = require('./middleware/error');
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
@@ -12,13 +13,13 @@ const express = require('express');
 const app = express();
 
 if (!config.get('jwtPrivateKey')) {
-  console.error('FATAL ERROR: jwtPrivateKey is not defined.');
-  process.exit(1);
+    console.error('FATAL ERROR: jwtPrivateKey is not defined.');
+    process.exit(1);
 }
 
 mongoose.connect('mongodb://localhost/vidly')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...'));
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
 app.use('/api/genres', genres);
@@ -27,6 +28,7 @@ app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
+app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
