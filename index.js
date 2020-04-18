c67fa466b9dd5939cc6cbda853369eafe28da619
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-19 01:18:45 +0200

    Extracted Routes, DB logic, Logging, Config, Validation from index.js into thier respective files in startup folder

--- index.js
+++ index.js
@@ -1,53 +1,12 @@
-require('express-async-errors');
 const winston = require('winston');
-require('winston-mongodb');
-const error = require('./middleware/error');
-const config = require('config');
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
-const mongoose = require('mongoose');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
 
-
-/*process.on('uncaughtException', ex => {
-    console.log("WE GOT AN UNCAUGHT EXCEPTION.");
-    winston.error(ex.message, ex);
-});*/
-winston.handleExceptions(new winston.transports.File({ filename: 'UncaughtExceptions.log' }));
-
-process.on('unhandledRejection', ex => {
-    throw ex; // Will throw uncaguht exception which will be catch by winston.
-});
-
-winston.add(winston.transports.File, { filename: 'logfile.log' });
-winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/vidly' });
-
-const p = Promise.reject(new Error("SOMETHING FAILED MISERABLY"));
-p.then(() => console.log("Done"));
-if (!config.get('jwtPrivateKey')) {
-    console.error('FATAL ERROR: jwtPrivateKey is not defined.');
-    process.exit(1);
-}
-
-mongoose.connect('mongodb://localhost/vidly', { useUnifiedTopology: true, useNewUrlParser: true })
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...'));
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use(error);
+require('./startup/logging');
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
+require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => winston.info(`Listening on port ${port}...`));
