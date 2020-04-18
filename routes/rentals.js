503835155cde2e781bb4ebc2339c5253c540f2f0
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-18 11:12:34 +0200

    ObjectId validation

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,27 +1,29 @@
-const { Rental, validate } = require('../models/rental');
-const { Movie } = require('../models/movie');
-const { Customer } = require('../models/customer');
-const mongoose = require('mongoose');
-const fawn = require('fawn');
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const { Customer } = require('../models/customer')
+const Fawn = require('fawn')
+const mongoose = require('mongoose')
+const { Movie } = require('../models/movie')
+const { Rental, validate } = require('../models/rental')
+
+Fawn.init(mongoose)
 
 router.get('/', async(req, res) => {
     const rentals = await Rental.find().sort('-dateOut');
     res.send(rentals);
-});
+})
+
 
 router.post('/', async(req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
+    // Makes sure the customerId/customer sends us is valid
     const customer = await Customer.findById(req.body.customerId);
-    if (!customer) return res.status(400).send('Invalid customer.');
+    if (!customer) return res.status(404).send('Invalid customerId');
 
     const movie = await Movie.findById(req.body.movieId);
-    if (!movie) return res.status(400).send('Invalid movie.');
-
-    if (movie.numberInStock === 0) return res.status(400).send('Movie not in stock.');
+    if (!movie) return res.status(404).send('Invalid movieId');
 
     let rental = new Rental({
         customer: {
@@ -34,26 +36,24 @@ router.post('/', async(req, res) => {
             title: movie.title,
             dailyRentalRate: movie.dailyRentalRate
         }
-    });
+    })
+
+    // This is for our success scenario
     try {
-        new Fawn().Task()
-            .save("rentals", rental)
-            .update("movies", { _id: movie._id }, { $inc: { numberInStock: -1 } })
+        // All args in here treated all together as unit
+        new Fawn.Task()
+            // First arg is collection we work with, and second is obj we wanna save
+            .save('rentals', rental)
+            // Update movies collection Second Arg is movie that should be updated Third is we increment the numInstock prop, and decrement by 1
+            .update('movies', { _id: movie._id }, {
+                $inc: { numberInStock: -1 }
+            })
             .run();
         res.send(rental);
-
     } catch (ex) {
-        res.status(500).send("Something went wrong.");
+        // 500 means Internal server error
+        res.status(500).send('Something failed.');
     }
-
-});
-
-router.get('/:id', async(req, res) => {
-    const rental = await Rental.findById(req.params.id);
-
-    if (!rental) return res.status(404).send('The rental with the given ID was not found.');
-
-    res.send(rental);
-});
+})
 
 module.exports = router;
