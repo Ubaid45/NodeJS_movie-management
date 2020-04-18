bbdbdc69b1f3031110f5646f8f62a77fdd5d4c9c
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-18 16:44:00 +0200

    Role based authorization

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
+//Get the current user
 router.get('/me', auth, async(req, res) => {
     const user = await User.findById(req.user._id).select('-password');
     res.send(user);
