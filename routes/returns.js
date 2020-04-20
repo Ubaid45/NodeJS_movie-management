af6930973ea2015b84a6787819f90680f219a561
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-20 15:07:25 +0200

    Looking up for an object

--- routes/returns.js
+++ routes/returns.js
@@ -1,14 +1,22 @@
 //const Joi = require('joi');
-/*const validate = require('../middleware/validate');
-const {Rental} = require('../models/rental');
-const {Movie} = require('../models/movie');
-const auth = require('../middleware/auth');*/
+//const validate = require('../middleware/validate');
+const { Rental } = require('../models/rental');
+//const {Movie} = require('../models/movie');
+//const auth = require('../middleware/auth');
 const express = require('express');
 const router = express.Router();
 
 router.post('/', async(req, res) => {
     if (!req.body.customerId) return res.status(400).send("Customer id not found");
     if (!req.body.movieId) return res.status(400).send("Movie id not found");
+
+    const rental = await Rental.findOne({
+        'customer._id': req.body.customerId,
+        'movie._id': req.body.movieId
+    });
+
+    if (!rental) return res.status(404).send('Rental not found.');
+
     res.status(401).send('Unauthorized');
 });
 
