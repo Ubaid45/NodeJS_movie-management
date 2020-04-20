e67f0139f7bad4539bf582af326606a55c4ef280
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-20 15:44:19 +0200

    Refactoring the validation logic

--- routes/returns.js
+++ routes/returns.js
@@ -1,5 +1,5 @@
-//const Joi = require('joi');
-//const validate = require('../middleware/validate');
+const Joi = require('joi');
+const validate = require('../middleware/validate');
 const moment = require('moment');
 const { Rental } = require('../models/rental');
 const { Movie } = require('../models/movie');
@@ -7,7 +7,7 @@ const auth = require('../middleware/auth');
 const express = require('express');
 const router = express.Router();
 
-router.post('/', auth, async(req, res) => {
+router.post('/', [auth, validate(validateReturn)], async(req, res) => {
     if (!req.body.customerId) return res.status(400).send("Customer id not found");
     if (!req.body.movieId) return res.status(400).send("Movie id not found");
 
@@ -31,5 +31,13 @@ router.post('/', auth, async(req, res) => {
     return res.status(200).send(rental);
 });
 
+function validateReturn(req) {
+    const schema = {
+        customerId: Joi.objectId().required(),
+        movieId: Joi.objectId().required()
+    };
+
+    return Joi.validate(req, schema);
+}
 
 module.exports = router;
