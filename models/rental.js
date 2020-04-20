ba16b87937b4130b0cf0d697498dd07664014a42
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-20 16:02:44 +0200

    Refactoring the domain logic

--- models/rental.js
+++ models/rental.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const Joi = require('joi');
 const mongoose = require('mongoose');
 const rentalSchema = new mongoose.Schema({
@@ -59,6 +60,13 @@ rentalSchema.statics.lookup = function(customerId, movieId) {
         'customer._id': customerId,
         'movie._id': movieId
     });
+}
+
+rentalSchema.methods.return = function() {
+    this.dateReturned = new Date();
+
+    const rentalDays = moment().diff(this.dateOut, 'days');
+    this.rentalFee = rentalDays * this.movie.dailyRentalRate;
 }
 
 const Rental = mongoose.model('Rental', rentalSchema);
