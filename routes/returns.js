4f3c46804efbf238149303378da0b3782e0f3fea
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-20 15:56:53 +0200

    Mongoose static methods

--- routes/returns.js
+++ routes/returns.js
@@ -11,10 +11,7 @@ router.post('/', [auth, validate(validateReturn)], async(req, res) => {
     if (!req.body.customerId) return res.status(400).send("Customer id not found");
     if (!req.body.movieId) return res.status(400).send("Movie id not found");
 
-    const rental = await Rental.findOne({
-        'customer._id': req.body.customerId,
-        'movie._id': req.body.movieId
-    });
+    const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
 
     if (!rental) return res.status(404).send('Rental not found.');
     if (rental.dateReturned) return res.status(400).send('Return already processed.');
